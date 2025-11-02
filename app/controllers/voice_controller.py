37d7baa1c28bae58540106a8f8d82300b1a6d7a5
abeni-al7 <abenezeralebachew3@gmail.com>
2025-11-02 14:08:36 +0300

    Handle exception on the /voice endpoint

--- app/controllers/voice_controller.py
+++ app/controllers/voice_controller.py
@@ -14,11 +14,14 @@ async def voice_controller(voice: UploadFile = File(...)):
     file = await voice.read()
     audio_service = AudioService(get_settings().EDEN_API_TOKEN, file)
     biblical_adviser = BiblicalAdvisor()
-    text_request = audio_service.speech_to_text()
-    english_request = audio_service.translate_text(text_request)
-    answer = biblical_adviser.generate_advice(english_request)
-    native_answer = biblical_adviser.translate(answer, "am")
-    audio_url = audio_service.text_to_speech(native_answer)
+    try:
+        text_request = audio_service.speech_to_text()
+        english_request = audio_service.translate_text(text_request)
+        answer = biblical_adviser.generate_advice(english_request)
+        native_answer = biblical_adviser.translate(answer, "am")
+        audio_url = audio_service.text_to_speech(native_answer)
+    except Exception as e:
+        return {"error": f"an error occured {e}"}
     return {
         "answer_url": audio_url,
         "text": native_answer
